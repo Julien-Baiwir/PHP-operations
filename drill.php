022b54b026d060fbd918a5229920a5e2ce1f8118
julienbaiwir <julien.freddy.baiwir@gmail.com>
2024-01-30 14:12:32 +0100

    1.2

--- drill.php
+++ drill.php
@@ -1,6 +1,6 @@
 <?php
 // 1.1 Clean your room Exercise 
-echo "test"
+
 // $room_is_filthy = true;
 
 // if( $room_is_filthy){
@@ -12,5 +12,19 @@ echo "test"
 // 	echo "<br>Nothing to do, room is neat.";
 // }
 
+// 1.2 Clean your room Exercise, improved
 
-;>
+// Create the array of possible states
+$possible_states = ['filthy', 'clean', 'okay'];
+
+// When testing, change the index value to navigate to the possible array values
+$room_filthiness = $possible_states[0]; 
+
+if( $room_filthiness[0] ){
+	echo "Yuk, Room is Disgusting! Let's clean it up !";
+} else if( $possible_states[2]){
+	echo "Yuk, Room is dirty : let's clean it up !";
+// ...
+} else {
+	echo "<br>Nothing to do, room is neat.";
+}
