cd41758482502cee976acf2fa57a604bcf51b329
julienbaiwir <julien.freddy.baiwir@gmail.com>
2024-01-30 14:12:24 +0100

    1.2

--- drill.php
+++ drill.php
@@ -1,16 +1,16 @@
 <?php
 // 1.1 Clean your room Exercise 
+echo "test"
+// $room_is_filthy = true;
 
-$room_is_filthy = true;
-
-if( $room_is_filthy){
-	echo "Yuk, Room is dirty : let's clean it up !";
-	cleanup_room();
-	echo "<br>Room is now clean!";
-	$room_is_filthy = false;
-} else {
-	echo "<br>Nothing to do, room is neat.";
-}
+// if( $room_is_filthy){
+// 	echo "Yuk, Room is dirty : let's clean it up !";
+// 	cleanup_room();
+// 	echo "<br>Room is now clean!";
+// 	$room_is_filthy = false;
+// } else {
+// 	echo "<br>Nothing to do, room is neat.";
+// }
 
 
 ;>
