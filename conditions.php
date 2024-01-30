ebc177a80688856fc2d47f6d33006ff0cc4208b2
julienbaiwir <julien.freddy.baiwir@gmail.com>
2024-01-30 13:50:39 +0100

    drill

--- conditions.php
+++ conditions.php
@@ -1,2 +1,22 @@
 <?php
- echo "test";
+if ($gender == 'Female'){
+
+	// Instructions here will be ran if gender is "Female"
+
+	if ( $age <= 12 and $language == "English" ) {
+		// Instructions here will be ran if gender is Female, age is below 13 and language is English.  
+
+	} else{
+		// Instructions here will be ran if gender is Female, and age is above 12 or language is not English. 
+
+	}
+
+} else {
+	// Instructions here will be ran if gender is not "Female"
+
+	if ( $age <= 12 and $language == "Chinese" ) {
+		// Instructions here will be ran if gender is not Female, and if age is below 13 and language is Chinese. 
+	
+	}
+}
+ ;
