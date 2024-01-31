e65a6053f843d8c93da0381a1b13766d370cdb70
julienbaiwir <julien.freddy.baiwir@gmail.com>
2024-01-31 20:43:56 +0100

    array

--- array/morearrays.php
+++ array/morearrays.php
@@ -1,3 +1,25 @@
 <?php 
-echo "test";
+$web_development = array (
+    'frontend'=> array(),
+    'backend' => array(),
+);
+
+$web_development ['frontend'] []= 'xhtml';
+$web_development ['backend'] []= 'Ruby';
+$web_development ['frontend'] []= 'CSS';
+$web_development ['backend'] []= 'Flash';
+$web_development ['frontend'] []= 'Javascript';
+
+echo '<pre>';
+print_r($web_development);
+echo '</pre>';
+
+$web_development ['frontend'][0]= 'html';
+// array_pop($web_development);
+// Remove the last element from the entire $web_development array
+unset($web_development['backend'][1]);
+
+echo '<pre>';
+print_r($web_development);
+echo '</pre>';
 ?>
